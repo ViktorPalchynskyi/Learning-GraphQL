acc30ea649d088f603377131f964a99662db8731
Viktor <palchinskij19@gmail.com>
2024-08-29 18:10:42 +0300

    29.08.24 - GQL queries in React

---
+++ graphQL_apollo_practice/client/components/SongList/SongList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+
+export default class SongList extends Component {
+    render() {
+        return <div>SongList</div>;
+    }
+}
+
+const query = gql`
+    {
+        songs {
+            title
+        }
+    }
+`;

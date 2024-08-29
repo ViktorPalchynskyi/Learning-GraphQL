40d32efea6687b67be3539398ad241ec1754f54f
Viktor <palchinskij19@gmail.com>
2024-08-29 21:13:11 +0300

    29.08.24 - adding mutations to react

--- graphQL_apollo_practice/client/components/SongCreate/SongCreate.js
+++ graphQL_apollo_practice/client/components/SongCreate/SongCreate.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongCreate extends Component {
     constructor() {
@@ -6,15 +8,27 @@ class SongCreate extends Component {
         this.state = { title: '' };
     }
 
+    onSubmit(event) {
+        event.preventDefault();
+        const { title } = this.state;
+
+        console.log(this.props);
+        this.props.mutate({
+            variables: {
+                title,
+            },
+        });
+    }
+
     render() {
         return (
             <div>
                 <h3>Create a new Song</h3>
-                <form>
-                    <lanel>Song Title:</lanel>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <label>Song Title:</label>
                     <input
                         onChange={(e) =>
-                            this.setState(e.target.value)
+                            this.setState({ title: e.target.value })
                         }
                         value={this.state.title}
                     />
@@ -24,4 +38,12 @@ class SongCreate extends Component {
     }
 }
 
-export default SongCreate;
+const mutation = gql`
+    mutation AddSong($title: String) {
+        addSong(title: $title) {
+            title
+        }
+    }
+`;
+
+export default graphql(mutation)(SongCreate);
